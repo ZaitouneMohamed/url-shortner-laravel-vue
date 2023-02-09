b4bda19a8b24627b85dc09e3fb1b3e288e7c20ae
mohamed zaitoune <mohamedztn37@gmail.com>
2023-02-09 14:47:01 +0100

    add pinia

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,15 @@
 import './bootstrap';
 
 import {createApp} from 'vue/dist/vue.esm-bundler';
+import { defineStore } from 'pinia'
+
 
 import home from './components/home.vue';
 
 const app = createApp({});
+const pinia = createPinia({});
 
 app.component('home-vue', home);
 
+app.use(pinia);
 app.mount('#app');
